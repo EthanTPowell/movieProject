afb7bb8f8457fac589c28de8f69adaa117b57169
EthanTPowell <ethantpowell00@gmail.com>
2022-08-19 21:28:49 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 const form = document.getElementById("form");
 const board = document.getElementById("board");
 const input = document.getElementById("input");
-const jumbo = document.getElementById('jumbo');
+const jumbo = document.getElementById("jumbo");
 
 form.addEventListener("submit", (e) => {
-    board.innerHTML = ``;
-    jumbo.innerHTML = ``;
-    e.preventDefault();
-    let inputString = input.value
-    fetch(`https://www.omdbapi.com/?s=${inputString}&apikey=4e21b89c`)
-        .then(response => response.json())
-        // .then(data => console.log(JSON.stringify(data, null, 2)))
-        // .then(data => console.log(data.Search))    
-        .then(data => data.Search.forEach(element => {
-            let imdb = element.imdbID
-            board.innerHTML += `<div id=><h3><button  onclick="inflate(${imdb})">${element.Title}</button></h3> <img src= "${element.Poster}"></div>`
-        }))
-    
-    form.reset()
+  board.innerHTML = ``;
+  jumbo.innerHTML = ``;
+  e.preventDefault();
+  let inputString = input.value;
+  fetch(`https://www.omdbapi.com/?s=${inputString}&apikey=4e21b89c`)
+    .then((response) => response.json(), (falseResponse) => alert(falseResponse))
+    // .then(data => console.log(JSON.stringify(data, null, 2)))
+    // .then(data => console.log(data.Search))
+    .then((data) =>
+      data.Search.forEach((element) => {
+        let imdb = element.imdbID;
+        console.log(imdb);
+        board.innerHTML += `<div id=><h3><button id="${imdb}" onclick="inflate(${imdb})">${element.Title}</button></h3> <img src= "${element.Poster}" class="posters"></div>`;
+      })
+    );
+
+  form.reset();
 });
 
 function inflate(imdb) {
-    jumbo.innerHTML = ``;
-    fetch(`https://www.omdbapi.com/?i=${imdb}&apikey=4e21b89c`)
-        .then(response => response.json())
-        .then(data=>console.log(data))
-    
-};
+  console.log(imdb.id);
+  jumbo.innerHTML = ``;
+  fetch(`https://www.omdbapi.com/?i=${imdb.id}&apikey=4e21b89c`)
+    .then((response) => response.json())
+        .then((data) => 
+        // {console.log(JSON.stringify(data, null, 2));
+           { jumbo.innerHTML += `<div class = "card"><div><h2>${data.Title}</h2></div><div><image src="${data.Poster}"></div></div><div class = "card"><div><h3>${data.Actors}</h3></div><div><h3>${data.Rated}</h3></div><div><h3>${data.Runtime}</h3></div></div>`
+        })
+}
